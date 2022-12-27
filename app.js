9f096009b7b6150168bed8e9af52014b22ce9251
Hifni <muhammad.gcs@gmail.com>
2022-12-27 17:48:51 +0700

    make it better

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // set the view engine to ejs
-app.use(express.static('views'));
-app.use(express.static('assets'));
+app.use(express.static("views"));
+app.use(express.static("assets"));
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
-  res.render('index.ejs');
+app.get("/", function (req, res) {
+  res.render("index.ejs");
 });
 
 // about page
-app.get('/blaster001', function(req, res) {
-  res.render('WA_blaster_whacenter.ejs');
+app.get("/blaster001", function (req, res) {
+  res.render("WA_blaster_whacenter.ejs");
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log("Server is listening on port http://localhost:8080");
